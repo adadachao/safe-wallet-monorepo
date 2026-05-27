8b3efb9395a83916485993c8198c675fdd92a563
adadachao <adadachao@gmail.com>
2026-05-27 19:09:42 +0800

    feat(web): apply MOVA theme and Mars chain configuration--

--- apps/web/src/components/sidebar/SidebarFooter/index.tsx
+++ apps/web/src/components/sidebar/SidebarFooter/index.tsx
@@ -1,29 +1,6 @@
 import { type ReactElement } from 'react'
 
 // MOVA: sidebar footer hidden (Use prod CGW, indexing status, What's new, Help)
-const SidebarFooter = (): ReactElement => null
+const SidebarFooter = (): ReactElement | null => null
 
 export default SidebarFooter
-
-// import { type ReactElement, useEffect } from 'react'
-// import { BEAMER_SELECTOR, loadBeamer } from '@/services/beamer'
-// import { useAppDispatch, useAppSelector } from '@/store'
-// import { CookieAndTermType, hasConsentFor } from '@/store/cookiesAndTermsSlice'
-// import { openCookieBanner } from '@/store/popupSlice'
-// import BeamerIcon from '@/public/images/sidebar/whats-new.svg'
-// import HelpCenterIcon from '@/public/images/sidebar/help-center.svg'
-// import { Divider, IconButton, ListItem, Stack, SvgIcon, Box } from '@mui/material'
-// import DebugToggle from '../DebugToggle'
-// import { IS_PRODUCTION } from '@/config/constants'
-// import Track from '@/components/common/Track'
-// import { OVERVIEW_EVENTS } from '@/services/analytics/events/overview'
-// import { MixpanelEventParams } from '@/services/analytics/mixpanel-events'
-// import { useCurrentChain } from '@/hooks/useChains'
-// import { HELP_CENTER_URL } from '@safe-global/utils/config/constants'
-// import IndexingStatus from '@/components/sidebar/IndexingStatus'
-//
-// const SidebarFooter = (): ReactElement => {
-//   ...
-// }
-//
-// export default SidebarFooter
